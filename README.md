5eb1b96747765c666f64a6286510184fe6a2497c
theobenoit <theo.benoit16@gmail.com>
2022-04-11 21:00:58 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Installation
+
+```bash
+yarn add @antipodes-medical/DoubleTapToGo
+```
+
+```javascript
+import '@antipodes-medical/DoubleTapToGo'
+```
+
+# Usage
+
+```javascript
+new DoubleTapToGo('.element', 'hover-class');
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-// Do something incredible
+/**
+ * Class to manage hover then click on touch devices
+ */
+export default class DoubleTapToGo {
+
+  constructor(selector, className = 'hover') {
+    //@formatter:off
+    if (!('ontouchstart' in window) && !navigator.msMaxTouchPoints && !navigator.userAgent.toLowerCase().match(/windows phone os 7/i)) {
+      return false;
+    }
+    //@formatter:on
+
+    document.querySelectorAll(selector).forEach(tapHover => {
+      tapHover.addEventListener('click', function (e) {
+        const tapHover = this;
+
+        if (tapHover.classList.contains(className)) {
+          return true;
+        } else {
+          const tapHovers = [...document.querySelectorAll(selector)];
+
+          tapHover.dispatchEvent(new Event('touchDeviceHover'));
+          tapHover.classList.add(className);
+          tapHovers.filter(element => {
+            if (element !== tapHover) {
+              element.dispatchEvent(new Event('touchDeviceOut'));
+              element.classList.remove(className);
+            }
+          });
+
+          e.preventDefault();
+          return false;
+        }
+      });
+    });
+  }
+
+}
